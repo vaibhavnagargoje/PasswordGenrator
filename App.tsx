9ce55d79945eeb02656ce95799d2429f84c7c963
vaibhav <vaibhavnagargoje2000@gmail.com>
2024-10-12 17:35:36 +0530

    styling done

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import { SafeAreaView, ScrollView, StyleSheet, Text, TextInput, Touchable, TouchableOpacity, View } from 'react-native'
+import { SafeAreaView, ScrollView, StyleSheet, Text, TextInput,TouchableOpacity, View } from 'react-native'
 import React, { useState } from 'react'
 
 import  BouncyCheckbox from 'react-native-bouncy-checkbox';
@@ -169,7 +169,7 @@ export default function App() {
             style={styles.secondaryBtn}
             onPress={()=>{
               handleReset();
-              resetPasswordState()
+              resetPassword()
             }}
             >
               <Text>
@@ -182,7 +182,15 @@ export default function App() {
      </Formik>
 
 
+      </View>
+
+      {isPassGenrated?(
+        <View style={[styles.card,styles.cardElevated]}>
+          <Text style={styles.subTitle}>Result: </Text>
+          <Text style={styles.description}>Long Press to Copy </Text>
+          <Text selectable={true} style={styles.generatedPassword}>{passowrd}</Text>
         </View>
+      ) : null}
 
       </SafeAreaView>
 
@@ -194,20 +202,98 @@ export default function App() {
 
 const styles = StyleSheet.create({
   appContainer:{
+    flex:1,
     
 
   },
   formContainer:{
+    margin:8,
+    padding:8,
 
   },
   title:{
+    fontSize:32,
+    fontWeight:'bold',
+    marginBottom:15,
 
   },
-  inputWrapper:{},
-  formAction:{},
-  inputColumn:{},
-  inputStyle:{},
-  heading:{},
-  errorText:{},
-  primaryBtn:{}
+  inputWrapper:{
+    marginBottom:15,
+    alignItems:'center',
+    justifyContent:'center',
+    flexDirection:'row'
+
+  },
+  formAction:{
+    flexDirection:'row',
+    justifyContent:'center'
+
+  },
+  inputColumn:{
+    flexDirection:'column'
+  },
+  inputStyle:{
+    padding:8,
+    width:'30%',
+    borderWidth:1,
+    borderRadius:4,
+    borderColor:'#ccc',
+  },
+  heading:{
+    fontSize:15,
+
+  },
+  errorText:{
+    fontSize:12,
+    color:'#ffod10'
+
+  },
+  primaryBtn:{
+   width:120,
+   padding:10,
+   borderRadius:8,
+   marginHorizontal:8,
+   backgroundColor:'#5DA3FA',
+  },
+  secondaryBtn:{
+    width:120,
+   padding:10,
+   borderRadius:8,
+   marginHorizontal:8,
+   backgroundColor:'#5DA3FA',
+
+  },
+  passGenerated:{},
+  passGeneratedText:{},
+  card:{
+    padding:12,
+    borderRadius:6,
+    marginHorizontal:12,
+  },
+  cardElevated:{
+    backgroundColor:"#ffffff",
+    elevation:1,
+    shadowOffset:{
+      width:1,
+      height:1,
+    },
+    shadowColor:"#333",
+    shadowOpacity:0.3,
+    shadowRadius:2,
+  },
+  subTitle:{
+    fontSize:26,
+    fontWeight:'600',
+    marginBottom:2,
+  },
+  description:{
+    color:'#758283',
+    marginBottom:8,
+  },
+  generatedPassword:{
+    fontSize: 22,
+    textAlign: 'center',
+    marginBottom: 12,
+    color:'#000'
+  },
 })
